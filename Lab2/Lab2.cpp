714898b4e768c715aefd9d1aff98e1aef1a4c913
Дима <@DESKTOP-COO5EJE>
2022-11-09 15:46:10 +0300

    Добавьте файлы проекта.

---
+++ Lab2/Lab2.cpp
@@ -0,0 +1,17 @@
+﻿#include <iostream>
+#include <cstdio>
+using namespace std;
+
+int main()
+{
+	double a, b, c;
+
+	cout << "Enter 1 number:";
+	cin >> a;
+	cout << "Enter 2 number:";
+	cin >> b;
+
+	c = a + b;
+
+	cout << c << endl;
+}
